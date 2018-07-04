01c53164be2d304d33e6362b2190de21a65695e4
Jonathan Nguyen <jnnj12@yahoo.com>
2018-07-04 03:26:48 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {
+  'My Bloody Valentine' : 'Phil Ochs'
+}
+
+function updatePlaylist (playlist, artistName, songTitle) {
+  return Object.assign(playlist, {[artistName]: songTitle})
+}
+
+function removeFromPlaylist (playlist, artistName) { 
+  return delete playlist.artistName
+}
